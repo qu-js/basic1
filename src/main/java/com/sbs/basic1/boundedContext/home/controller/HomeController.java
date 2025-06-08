20e05ee8abe4200fa913646114d433ee63a01878
qu-js <fsung2896@gmail.com>
2025-06-08 17:56:23 +0900

    home/increase

--- src/main/java/com/sbs/basic1/boundedContext/home/controller/HomeController.java
+++ src/main/java/com/sbs/basic1/boundedContext/home/controller/HomeController.java
@@ -7,11 +7,30 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 // mvc패턴을 사용하여 아래 클래스를 controller 클래스로 만들겠다는 의미
 public class HomeController {
-    @GetMapping("home/main")
+    int num;
+
+    public HomeController() {
+        num=0;
+    }
+
+
+    @GetMapping("/home/main")
     // "/home/main" 요청이 들어오면 아래의 메소드를 실행시키라는 의미
     @ResponseBody
     // 아래 메서드가 실행된 결과를 body의 response로 보내라는 의미
     public String showHomeMain() {
         return "Hello Wolrd!";
+    }
+
+    @GetMapping("/home/main2")
+    @ResponseBody
+    public String showHomeMain2() {
+        return "Nice to meet you";
+    }
+
+    @GetMapping("/home/increase")
+    @ResponseBody
+    public int showincrease() {
+        return num++;
     }
 }
